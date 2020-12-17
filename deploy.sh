84afde8fe945b1c197b551f0fb0d4fc705548c42
wengzhisong <1322457349@qq.com>
2020-12-17 15:42:24 +0800

    添加发布功能

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env sh
 
 set -e
+# 推送到 master
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
+
+# 推送到gh-pages
+git branch -D gh-pages || git branch gh-pages
+git branch gh-pages || git checkout gh-pages
+git merge master
+
 npm run build
 cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
 git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git gh-pages
+
+git checkout master
 cd -
