a641b198ef3ba72866e87de2d223dd05677d2ed6
wengzhisong <1322457349@qq.com>
2020-12-17 15:27:19 +0800

    发布到gh-pages

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
+git branch -D gh-pages
+git checkout -b ghpages
+git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git gh-pages
+git checkout master
 cd -
