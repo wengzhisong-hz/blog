5a7d073cb97a7a1f07e44121f7e8b51a0f6b0909
wengzhisong <1322457349@qq.com>
2020-12-17 16:09:51 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
-
 set -e
+
+git add .
+git commit -m 'deploy'
+
 npm run build
 cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
+
+cd ../../
+git add .
+git commit -m 'deploy'
+git checkout dev || git checkout -b dev
+git merge master
+git push origin dev
+git chekout master
 cd -
